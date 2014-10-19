0d4e5eca658b48f7f6774ebba818f16d6ca7455e
paqao <pacco.inter@gmail.com>
2014-10-19 16:59:32 +0200

    CisLessonI finito

---
+++ PartII/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Przestrzenie nazw piszemy z dużych liter
+namespace PartII
+{
+    // nazwy klas również
+    class Program
+    {
+        /*
+         * pole prywatne -dostępne jedynie wewnątrz klasy
+         * statyczne - wszystkie instance mają taką samą wartość tego pola
+         * int - typ pola
+         * standard podkreślenie i później wyraz z małej
+         */
+        private static int _counter = 0; // licznik
+        // Nazwy metod - duże litery :), argumenty CamelCase
+        static void Main(string[] args)
+        {
+            string providedCommand = "";
+
+            while (providedCommand != "exit") // pracujemy dopóki wprowadzony string będzie różny od exit
+            {
+                _counter ++;
+                Console.Write("{0} >> ", _counter);
+                providedCommand = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ PartIII/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartIII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Korzystając z wiedzy zdobytej dzisiaj na spotkaniu stwórz
+             * swego rodzaju "brainf*ck", który posiada polecenia
+             * * inc - zwiększa licznik o jeden
+             * * dec - zmniejsza licznik o jeden
+             * * print - wypisuje wartość licznika
+             * * save [liczba] - wprowadza nową wartość z "liczba"
+             *      Int.TryParse
+             * * zero - zeruje licznik
+             * * exit - wychodzi z programu
+             * Program powinien zachowywać standardy nazewnictwa.
+             */
+        }
+    }
+}
